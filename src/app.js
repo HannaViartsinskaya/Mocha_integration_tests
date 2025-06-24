47c6c71692d263f601820412d88207dfac1f31ab
Hanna Nasi <hanna.nasi@exmox.com>
2025-06-24 06:55:32 +0200

    Mocha proj setup

---
+++ src/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+const users = [
+    { id: 1, name: 'John Doe' },
+    { id: 2, name: 'Jane Doe' }
+];
+
+app.get('/api/users', (req, res) => {
+    res.status(200).json(users);
+});
+
+app.post('/api/user', (req, res)=>{
+    const {name} = req.body;
+    if(!name){
+        return res.status(400).json({error:'The user name is missing'});
+    }
+    const newUser={
+        id:users.length+1,
+        name:name
+    };
+    users.push(newUser);
+    res.status(201).json(newUser);
+})
+
+module.exports = app;
---
+++ test/ api.test.js
@@ -0,0 +1,40 @@
+const chai = require('chai');
+const expect = chai.expect;
+const supertest = require('supertest');
+const app = require('../src/app');
+
+describe('GET /api/users', function() {
+    it('should return a list of users', async function() {
+        const res = await supertest(app)
+            .get('/api/users')
+            .expect(200);
+
+        expect(res.body).to.be.an('array');
+        expect(res.body).to.have.length.greaterThan(0); // Check if array is not empty
+    });
+});
+
+describe('POST /api/user', function (){
+    it ('should add a new user', async function(){
+        const newUser={
+            name:"Hanna Test"
+        }
+        const res= await supertest(app)
+            .post('/api/user')
+            .send(newUser)
+            .expect(201)
+
+        expect(res.body).to.have.property('id');
+        expect(res.body.name).to.equal(newUser.name);
+    });
+
+    it('should be error if users name is not provided', async function(){
+        const newUser = {};
+        const res=await supertest(app)
+            .post('/api/user')
+            .send(newUser)
+            .expect(400)
+        expect(res.body.error).to.equal('The user name is missing')
+        expect(res.body).to.have.property('error')
+    });
+});
